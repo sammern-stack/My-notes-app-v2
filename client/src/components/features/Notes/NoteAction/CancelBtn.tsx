a0c61187feef24621e77d8e5457fec6038d169c1
sammern <samreactdev@gmail.com>
2026-06-26 16:31:38 +0300

    fix(notes): fix cancel button to correctly go back before changing anything

--- client/src/components/features/Notes/NoteAction/CancelBtn.tsx
+++ client/src/components/features/Notes/NoteAction/CancelBtn.tsx
@@ -1,17 +1,34 @@
 // ——— Imports —————————————————————————————————————————————————————————————————————————————————————
-import { useEditorStore } from "@stores";
+import { useEditorStore, useNotesStore } from "@stores";
 
 // ——— Component ———————————————————————————————————————————————————————————————————————————————————
 export const CancelBtn = () => {
+  const editorState = useEditorStore((s) => s.editorState);
   const setEditorState = useEditorStore((s) => s.setEditorState);
+  const selectedNoteId = useEditorStore((s) => s.selectedNoteId);
+  const setActiveNote = useEditorStore((s) => s.setActiveNote);
   const setSelectedNoteId = useEditorStore((s) => s.setSelectedNoteId);
   const cashedSelectedId = useEditorStore((s) => s.cashedSelectedId);
   const setCashedSelectedId = useEditorStore((s) => s.setCashedSelectedId);
+  const fetchNote = useNotesStore((s) => s.fetchNote);
 
-  const handleCancel = () => {
-    setEditorState("updating");
-    setSelectedNoteId(cashedSelectedId);
-    setCashedSelectedId(null);
+  const handleCancel = async () => {
+    if (editorState === "creating") {
+      setEditorState("updating");
+      setSelectedNoteId(cashedSelectedId);
+      setCashedSelectedId(null);
+    }
+
+    if (selectedNoteId && editorState === "updating") {
+      const prevNote = await fetchNote(selectedNoteId);
+      setActiveNote({
+        title: prevNote.title,
+        tags: prevNote.tags.join(", "),
+        content: prevNote.content,
+        createdAt: prevNote.createdAt,
+        updatedAt: prevNote.updatedAt,
+      });
+    }
   };
 
   return (
