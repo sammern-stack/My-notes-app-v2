a8831057eb0a2cb0776a7b222468d372eef3008c
sammern <samreactdev@gmail.com>
2026-06-30 15:00:30 +0300

    refactor(archiveBtn): update content based on isArchived flag and open dialog

--- client/src/components/features/Notes/NoteAction/ArchiveBtn.tsx
+++ client/src/components/features/Notes/NoteAction/ArchiveBtn.tsx
@@ -1,14 +1,23 @@
 // ——— Imports —————————————————————————————————————————————————————————————————————————————————————
+import { useDialogStore, useEditorStore } from "@stores";
 import { LabelWithIcon } from "@/components/shared";
 
 // ——— Component ———————————————————————————————————————————————————————————————————————————————————
 export const ArchiveBtn = () => {
+  const { isArchived } = useEditorStore((s) => s.activeNote);
+  const setDialogIsOpen = useDialogStore((s) => s.setDialogIsOpen);
+  const setDialogPurpose = useDialogStore((s) => s.setDialogPurpose);
+
   return (
     <LabelWithIcon
       as="button"
       className="note__action note__action--archive"
-      label="Archive Note"
-      icon="icon-archive"
+      label={isArchived ? "Restore Note" : "Archive Note"}
+      icon={isArchived ? "icon-restore" : "icon-archive"}
+      onClick={() => {
+        setDialogPurpose(isArchived ? "restore" : "archive");
+        setDialogIsOpen(true);
+      }}
     />
   );
 };
