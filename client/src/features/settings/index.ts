eadcdf93cee086e933b5feed7e8fa8ee83145535
sammern <samreactdev@gmail.com>
2026-09-25 22:38:58 +0300

    refactor: remove TagsList component and integrate tag filtering directly into Sidebar for improved clarity

--- client/src/features/settings/index.ts
+++ client/src/features/settings/index.ts
@@ -1,3 +1,2 @@
 export { RenderOptions } from "./components/RenderOptions/RenderOptions";
 export { SearchBar } from "./components/SearchBar/SearchBar";
-export { TagsList } from "./components/TagsList/TagsList";
--- client/src/layout/Sidebar/Sidebar.tsx
+++ client/src/layout/Sidebar/Sidebar.tsx
@@ -1,9 +1,22 @@
 import styles from "./Sidebar.module.scss";
-import { Link } from "react-router-dom";
-import { RenderOptions, TagsList } from "@/features/settings";
+import { Link, useLocation } from "react-router-dom";
+import { RenderOptions } from "@/features/settings";
+import { useGetNotes } from "@/features/notes";
+import { useFiltersStore } from "@/shared/stores";
+
 import LogoIcon from "@/assets/images/logo.svg?react";
+import TagIcon from "@/assets/images/icon-tag.svg?react";
+import ChevronRightIcon from "@/assets/images/icon-chevron-right.svg?react";
 
 export const Sidebar = () => {
+  const location = useLocation();
+  const { data: notes = [] } = useGetNotes();
+  const { addTagFilter, removeTagFilter } = useFiltersStore.getState();
+  const tags = [...new Set(notes.flatMap((note) => note.tags))].sort((a, b) =>
+    a.localeCompare(b),
+  );
+  const tagFilters = useFiltersStore((s) => s.tagFilters);
+
   return (
     <div className={styles.sidebar}>
       <div className={styles["sidebar__logo-wrapper"]}>
@@ -17,7 +30,30 @@ export const Sidebar = () => {
         <div className={styles["sidebar__tags-header"]}>
           <div className={styles["sidebar__tags-title"]}>Tags</div>
         </div>
-        <TagsList />
+        <div className={styles.sidebar__tags}>
+          {tags.map((tag) => {
+            const IsSettingsPage = location.pathname === "/settings";
+            const isActive = IsSettingsPage ? false : tagFilters.includes(tag);
+
+            const tagClasses = [
+              styles.sidebar__tag,
+              isActive && styles["sidebar__tag--active"],
+            ].join(" ");
+
+            const handleToggleTag = () => {
+              if (IsSettingsPage) return;
+              if (tagFilters.includes(tag)) return removeTagFilter(tag);
+              return addTagFilter(tag);
+            };
+
+            return (
+              <button className={tagClasses} onClick={handleToggleTag}>
+                <TagIcon /> <p>{tag}</p>
+                {isActive && <ChevronRightIcon />}
+              </button>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
