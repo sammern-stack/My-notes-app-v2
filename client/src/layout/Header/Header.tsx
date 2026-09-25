0730f44c4af9840943e6a44eaaac156b15827ba0
sammern <samreactdev@gmail.com>
2026-09-25 23:18:42 +0300

    refactor: remove SearchBar component; integrate search functionality directly into Header for improved clarity

--- client/src/layout/Header/Header.tsx
+++ client/src/layout/Header/Header.tsx
@@ -1,16 +1,23 @@
-import { PageTitle, LabelWithIcon } from "@/shared/components";
-import { SearchBar } from "@/features/settings";
-import SettingsIcon from "@/assets/images/icon-settings.svg?react";
-
 import styles from "./Header.module.scss";
 import { Link } from "react-router-dom";
+import { PageTitle, LabelWithIcon } from "@/shared/components";
+
+import SettingsIcon from "@/assets/images/icon-settings.svg?react";
+import SearchIcon from "@/assets/images/icon-search.svg?react";
 
 export const Header = () => (
   <div className={styles["page__header"]}>
     <PageTitle />
 
     <div className={styles["page__header-content"]}>
-      <SearchBar />
+      <div className={styles["page__search"]}>
+        <SearchIcon />
+        <input
+          type="text"
+          className={styles["page__search-input"]}
+          placeholder="Search by title, content, or tags…"
+        />
+      </div>
 
       <Link to="/settings">
         <LabelWithIcon
